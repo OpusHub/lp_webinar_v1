04ff0fdccb0af9cd61b85f1d907c6e6f9a54f072
Victor Yulo Beirao <yulo@MacBook-Air-de-Victor.local>
2025-12-02 17:35:27 -0300

    feat: replace GTM with Google Tag (gtag.js) AW-17774738308

--- web-app/app/layout.tsx
+++ web-app/app/layout.tsx
@@ -29,30 +29,26 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PSK6NRHN');`,
-          }}
+        {/* Google Tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17774738308"
+          strategy="afterInteractive"
         />
+        <Script id="google-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-17774738308');
+          `}
+        </Script>
       </head>
       <body
         className={`${montserrat.variable} ${roboto.variable} antialiased bg-background text-foreground`}
       >
-        {/* Google Tag Manager (noscript) - Must be immediately after opening body tag */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PSK6NRHN"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
+
 
         <Script id="fb-pixel" strategy="afterInteractive">
           {`
