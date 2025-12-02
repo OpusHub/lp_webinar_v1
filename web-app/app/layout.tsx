04af6e902ea65cfd3a0e985df901b8caffb097c5
Victor Yulo Beirao <yulo@MacBook-Air-de-Victor.local>
2025-12-02 17:02:50 -0300

    fix: cleanup GTM implementation with raw script tag

--- web-app/app/layout.tsx
+++ web-app/app/layout.tsx
@@ -29,16 +29,16 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        {/* Google Tag Manager - Must be in head as high as possible */}
-        <Script id="gtm" strategy="beforeInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PSK6NRHN');
-          `}
-        </Script>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PSK6NRHN');`,
+          }}
+        />
       </head>
       <body
         className={`${montserrat.variable} ${roboto.variable} antialiased bg-background text-foreground`}
