10fbd4cd54015002ac62c3d7db6e4be6b8f69fce
Victor Yulo Beirao <yulo@MacBook-Air-de-Victor.local>
2025-12-03 00:29:16 -0300

    fix: manual GTM implementation with dangerouslySetInnerHTML in head

--- web-app/app/layout.tsx
+++ web-app/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Montserrat, Roboto } from "next/font/google";
-import { GoogleTagManager } from '@next/third-parties/google'
 import Script from "next/script";
 import "./globals.css";
 
@@ -28,12 +27,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      {/* Google Tag Manager - Componente Oficial */}
-      <GoogleTagManager gtmId="GTM-PSK6NRHN" />
-
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PSK6NRHN');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${montserrat.variable} ${roboto.variable} antialiased bg-background text-foreground`}
       >
+        {/* GTM noscript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PSK6NRHN"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         {/* Meta Pixel */}
         <Script id="fb-pixel" strategy="afterInteractive">
           {`!function(f,b,e,v,n,t,s)
