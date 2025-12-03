2657bea3a778e830d8b304f99c67bf65e3313530
Victor Yulo Beirao <yulo@MacBook-Air-de-Victor.local>
2025-12-02 21:17:17 -0300

    fix: remove GTM, install Google Ads tag (AW-17774738308)

--- web-app/app/layout.tsx
+++ web-app/app/layout.tsx
@@ -31,26 +31,20 @@ export default function RootLayout({
       <body
         className={`${montserrat.variable} ${roboto.variable} antialiased bg-background text-foreground`}
       >
-        {/* Google Tag Manager - DEVE VIR PRIMEIRO */}
-        <Script id="gtm-script" strategy="afterInteractive">
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17774738308"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-config" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PSK6NRHN');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17774738308');
           `}
         </Script>
-
-        {/* GTM noscript */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PSK6NRHN"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
 
         {/* Meta Pixel */}
         <Script id="fb-pixel" strategy="afterInteractive">
