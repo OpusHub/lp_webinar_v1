cce2968af7266090b53e65f53f78905921bbab4e
Victor Yulo Beirao <yulo@MacBook-Air-de-Victor.local>
2025-12-02 22:28:01 -0300

    fix: use official GoogleTagManager component from @next/third-parties

--- web-app/app/layout.tsx
+++ web-app/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat, Roboto } from "next/font/google";
+import { GoogleTagManager } from '@next/third-parties/google'
 import Script from "next/script";
 import "./globals.css";
 
@@ -27,41 +28,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      {/* Google Tag Manager - beforeInteractive injeta no head automaticamente */}
-      <Script id="gtm-script" strategy="beforeInteractive">
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-PSK6NRHN');`}
-      </Script>
-
-      {/* Meta Pixel */}
-      <Script id="fb-pixel" strategy="afterInteractive">
-        {`!function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1310751963807969');
-        fbq('track', 'PageView');`}
-      </Script>
+      {/* Google Tag Manager - Componente Oficial */}
+      <GoogleTagManager gtmId="GTM-PSK6NRHN" />
 
       <body
         className={`${montserrat.variable} ${roboto.variable} antialiased bg-background text-foreground`}
       >
-        {/* GTM noscript */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PSK6NRHN"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
+        {/* Meta Pixel */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1310751963807969');
+          fbq('track', 'PageView');`}
+        </Script>
 
         {children}
       </body>
